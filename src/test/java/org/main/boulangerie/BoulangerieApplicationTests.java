9fe59d80e6058169c363c93e186f65ae1a9aaa59
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2024-12-26 08:27:41 +0300

    initialize

---
+++ src/test/java/org/main/boulangerie/BoulangerieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main.boulangerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulangerieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
