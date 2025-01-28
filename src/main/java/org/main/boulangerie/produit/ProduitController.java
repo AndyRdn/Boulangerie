01edab237e175f57abd2f881bab5cb2434a53756
Odilah <odilahrandrianjanahary@hotmail.com>
2025-01-28 14:53:12 +0300

    Revert " df"
    This reverts commit c056296a3d8c14e1ab9f7db1559633f7ed20352d.

--- src/main/java/org/main/boulangerie/produit/ProduitController.java
+++ src/main/java/org/main/boulangerie/produit/ProduitController.java
@@ -80,7 +80,7 @@ public class ProduitController {
 
     @PostMapping("/update")
     public String update(@RequestParam("id") Integer id, @ModelAttribute ProduitForm form) {
-        produitService.update(id, form);
+//        produitService.update(id, form);
         return "redirect:/produit/list";
     }
 
